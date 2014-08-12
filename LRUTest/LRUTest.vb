af28328ba97994c2fb14d6ba4d4bc015a92e9c61
CosX <karl.f91@gmail.com>
2014-08-12 12:05:42 +0200

    cache

---
+++ LRUTest/LRUTest.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Text
+Imports LRU
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class LRUTest
+
+    <TestMethod()> Public Sub AddToCache()
+        Dim lru As New LruCache(Of String, String)
+        lru.MaxSize = 5
+        lru.Add("String1", "1")
+        lru.Add("String2", "2")
+        lru.Add("String3", "3")
+
+        Assert.AreEqual(lru.Count, 3)
+    End Sub
+
+    <TestMethod()> Public Sub MaxSizeLimit()
+        Dim lru As New LruCache(Of String, String)
+        lru.MaxSize = 5
+        lru.Add("String1", "1")
+        lru.Add("String2", "2")
+        lru.Add("String3", "3")
+        lru.Add("String4", "4")
+        lru.Add("String5", "5")
+        lru.Add("String6", "6")
+
+        Assert.AreEqual(lru.Count, 5)
+    End Sub
+
+    <TestMethod()> Public Sub SetValue()
+        Dim lru As New LruCache(Of String, String)
+        lru.MaxSize = 5
+        lru.Add("String1", "1")
+        lru.Add("String2", "2")
+        lru.Add("String3", "3")
+        lru.Add("String4", "4")
+
+        lru.Item("String3") = "Jeg er modda!"
+
+        Assert.AreEqual(lru.Item("String3"), "Jeg er modda!")
+    End Sub
+
+End Class
